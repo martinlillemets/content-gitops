221dfe1794fbd7bdaeadbf1a78ede17fcdd713e8
Martin Lillemets <martinlillemets@users.noreply.github.com>
2022-03-04 13:32:44 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Is there something wrong with this file?"
+    return "Hello World! Trigger rebuild because we changed requirements withhout pull request like a dummy"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
